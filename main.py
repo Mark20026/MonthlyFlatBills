29f0f525541b78fa4f51cbb87ff29f504322ff18
Flatmates_Bill <bogimark2002@gmail.com>
2023-08-20 19:47:34 +0200

    Added a pattern to the bill_period to control the input

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
+import re
+
 from flat import Bill, Flatmate
 from pdf_report import GeneratePaymentPDF
 
 bill_amount = float(input("How much is the bill?: "))
-bill_period = input("What month and year is it? (August 2023): ").split()
 
-period_month = bill_period[0]
-period_year = bill_period[1]
+while True:
+    bill_period = input("What month and year is it? (August 2023): ")
+
+    # Regular expression pattern for "Month Year" format (e.g., August 2023)
+    pattern = r"^(January|February|March|April|May|June|July|August|September|October|November|December)\s+\d{4}$"
+
+    if re.match(pattern, bill_period):
+        # Extract month and year
+        month, year = bill_period.split()
+        print("Month:", month)
+        print("Year:", year)
+        break
+    else:
+        print("Invalid input. Please provide the month and year in the format 'Month Year' (e.g., August 2023).")
 
 flatmate_count = int(input("How many people live there?: "))
 
@@ -15,9 +28,9 @@ for i in range(flatmate_count):
     flatmate_days_in = int(input(f"How many days was the {i+1}. flatmate been in: "))
     flatmates.append(Flatmate(flatmate_name, flatmate_days_in))
 
-bill = Bill(bill_amount, period_year, period_month)
+bill = Bill(bill_amount, year, month)
 
-bill.days_in_period(period_month)
+bill.days_in_period(month)
 
 pdf_of_payment = GeneratePaymentPDF(filename=f"{bill.month + ' ' + bill.year}.pdf")
 print("The bill of the payments is printing")
